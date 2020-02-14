5851947d1b123bfd3551ca9c1fda6278842d2193
Daniel <daniel@Daniels-MacBook-Pro.local>
2020-02-13 19:24:13 -0500

    created even more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,21 +6,23 @@
 let cards = ["queen", "queen", "king", "king"];
 
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
+
 
 cardsInPlay.push(cardOne);
 cardsInPlay.push(cardTwo);
 
-if(cardsInPlay.length === 2)
-{
-  if(cardsInPlay[0] === cardsInPlay[1])
-  {
-    alert("Match!");
-  }
-  else {
 
-  }
+
+
+
+function checkForMatch()
+{
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+    }
+    else {
+      console.log("Sorry, try again.");
+    }
 }
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
